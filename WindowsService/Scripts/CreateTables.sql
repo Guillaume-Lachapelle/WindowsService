fde4934d925e98d2e521e1248942375158a0eeae
Guillaume-Lachapelle <guillaume2.lachapelle@gmail.com>
2023-07-03 23:00:47 -0400

    Fixed LastName type in teachers table

--- WindowsService/Scripts/CreateTables.sql
+++ WindowsService/Scripts/CreateTables.sql
@@ -1,5 +1,5 @@
 ﻿-- Create table Teachers if it does not exist
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Teachers' AND xtype='U') CREATE TABLE Teachers(ID nchar(9) not null,FirstName nvarchar(max) not null, LastName nchar(10) not null,
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Teachers' AND xtype='U') CREATE TABLE Teachers(ID nchar(9) not null,FirstName nvarchar(max) not null, LastName nvarchar(max) not null,
 							Department nchar(4) null,Salary int not null, HiringDate datetime not null, SchoolEmail nvarchar(max) not null, Classes nvarchar(max) null);
 
 -- Create table Students if it does not exist
